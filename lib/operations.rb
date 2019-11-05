7ba6c9e2ed1ac29d2935bfbdc24d55e7adbff0e5
Mary Arbachauskas <mary.arbachauskas@gmail.com>
2019-11-05 15:38:06 +0000

    Automatically backed up by Learn

--- lib/operations.rb
+++ lib/operations.rb
@@ -1,14 +1,5 @@
-def unsafe?(speed)
-answer="unsafe?"
-speed=0
-if 40>speed>60
-	return true
-elsif speedm>=40
-	return false
-else
- speed =< 60
- return false
-	end
+def unsafe? speed
+  speed < 40 || speed > 60
 end
 
 
